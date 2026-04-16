b32eeeb26cfbd849984b8e273fe777ba9f2dcbbc
rishika <rishika@C010422930.zipangu.cac.co.jp>
2026-04-16 14:13:26 +0900

    やった２

--- java_comprehension_exercises_volume1/src/basic/question06/Question06_1.java
+++ java_comprehension_exercises_volume1/src/basic/question06/Question06_1.java
@@ -6,7 +6,10 @@ public class Question06_1 {
 
 	public static void main(String[] args) throws IOException {
 
-		// TODO:ここに実装
-		
+		String[] messgage = { "こんにちは", "Java学習中", "繰り返しの演習", "頑張ります" };
+		for (int i = 0; i < 4; i++) {
+			System.out.println(messgage[i]);
+		}
+		System.out.println("繰り返しが終了しました。");
 	}
 }
